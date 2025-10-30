880671c5b253c9ee0fc3ef35ce6c69a72693a0bc
msjangid <71596902+msjangid@users.noreply.github.com>
2025-10-30 19:09:04 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -190,7 +190,7 @@ class ProductGrid {
         this.grid = document.getElementById('productGrid');
         this.products = [
             { name: 'Bed Room', image: 'Assets/images/bd1.jpg', price: 'Custom' },
-            { name: 'Bed Room', image: 'Assets/images/Page36.JPG', price: 'Custom' },
+            { name: 'Bed Room', image: 'Assets/images/Page36.jpg', price: 'Custom' },
             { name: 'Bed Room', image: 'Assets/images/bd3.jpg', price: 'Custom' },
             { name: 'Bed Room', image: 'Assets/images/Page43.jpg', price: 'Custom' },
             { name: 'Dining Table', image: 'Assets/images/D1.JPG', price: 'Custom' },
