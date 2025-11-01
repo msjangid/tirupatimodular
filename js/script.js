8a006482b7f53635aea77a109408396e1ab2ff0c
msjangid <71596902+msjangid@users.noreply.github.com>
2025-11-01 22:23:09 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -198,7 +198,7 @@ class ProductGrid {
             { name: 'Kitchen', image: 'Assets/images/Page19.jpg', price: 'Custom' },
             { name: 'Kitchen', image: 'Assets/images/k2.jpg', price: 'Custom' },
             { name: 'Kitchen', image: 'Assets/images/Page51.jpg', price: 'Custom' },
-            { name: 'Living Room', image: 'Assets/images/Page20.JPG', price: 'Custom' },
+            { name: 'Living Room', image: 'Assets/images/Page20.jpg', price: 'Custom' },
             { name: 'Living Room', image: 'Assets/images/Page2.jpg', price: 'Custom' },
             { name: 'Living Room', image: 'Assets/images/Page3.jpg', price: 'Custom' },
             { name: 'Room Ceilling', image: 'Assets/images/Page5.jpg', price: 'Custom' },
